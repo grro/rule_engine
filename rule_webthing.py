894de3d4311fc6b6ac91bc14fcea8ebec1303159
grro <gregor.roth@1und1.de>
2024-06-24 07:14:46 +0200

    webthing support added

---
+++ rule_webthing.py
@@ -0,0 +1,46 @@
+import logging
+import tornado.ioloop
+from webthing import (SingleThing, Property, Thing, Value, WebThingServer)
+from device import DeviceManager
+
+
+
+
+class RuleThing(Thing):
+
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, description: str, device_manager: DeviceManager):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:device_manager-1',
+            'DeviceManager',
+            ['MultiLevelSensor'],
+            description
+        )
+        self.ioloop = tornado.ioloop.IOLoop.current()
+        self.device_manager = device_manager
+        self.device_manager.add_change_listener(self.on_value_changed)
+
+        self.devices = Value(self.__devicenames)
+        self.add_property(
+            Property(self,
+                     'devices',
+                     self.devices,
+                     metadata={
+                         'title': 'devices',
+                         "type": "sting",
+                         'description': 'comma separated list of device names',
+                         'readOnly': True,
+                     }))
+
+    @property
+    def __devicenames(self) -> str:
+        return ", ".join(sorted([device.name for device in self.device_manager.devices]))
+
+    def on_value_changed(self, name: str):
+        self.ioloop.add_callback(lambda: self._on_value_changed(name))
+
+    def _on_value_changed(self, name):
+        self.devices.notify_of_external_update(self.__devicenames)
