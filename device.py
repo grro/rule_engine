be37da9316bb4d5e64c0aaa43bf9f330aeb9699d
grro <gregor.roth@1und1.de>
2024-10-04 06:25:42 +0200

    logging improved

--- device.py
+++ device.py
@@ -155,7 +155,7 @@ class Webthing(Device, Listener):
                 resp = self.__session.put(property_uri, data=data, timeout=10)
                 if resp.status_code == 200:
                     self._properties[prop_name] = value
-                    logging.info(self.uri + " updated: " + prop_name + "=" + str(value) + ("" if reason is None else " (" + reason + ")"))
+                    logging.info(self.name + " (" + self.uri + ") updated: " + prop_name + "=" + str(value) + ("" if reason is None else " (" + reason + ")"))
                 else:
                     logging.info(self.name + " calling " + self.uri + " to update " + prop_name + " with " + str(value) + " failed. Got " + str(resp.status_code) + " " + resp.text)
                 self._notify_listener({prop_name: value})
